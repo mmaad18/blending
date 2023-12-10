e14b358ff5c6e90f059801ce02e008626e64fa4c
mmaad18 <61145345+mmaad18@users.noreply.github.com>
2023-12-10 04:00:42 +0100

    Added second array to be generated. Now needs pipes between first and second array

--- convolution.py
+++ convolution.py
@@ -1,4 +1,6 @@
 import bpy
+import random
+
 
 def create_material(name, color, alpha):
     """Create a new material with specified color and alpha."""
@@ -9,27 +11,32 @@ def create_material(name, color, alpha):
     bsdf.inputs["Alpha"].default_value = alpha
     return material
 
-def create_box(location, material):
-    """Create a single cube at a specified location and assign a material to it."""
-    bpy.ops.mesh.primitive_cube_add(size=1, enter_editmode=False, align='WORLD', location=location)
+
+def create_box(location, material, size=1):
+    """Create a single cube at a specified location, size, and assign a material to it."""
+    bpy.ops.mesh.primitive_cube_add(size=size, enter_editmode=False, align='WORLD', location=location)
     obj = bpy.context.active_object
     obj.data.materials.append(material)
 
-def create_row_of_boxes(number_of_boxes, spacing, color, alpha):
+
+def create_array_of_boxes(number_of_boxes, spacing, pos_0=(0,0,0), alpha=1):
     """Create a row of boxes with specified spacing and material."""
-    material = create_material("BoxMaterial", color, alpha)
     for i in range(number_of_boxes):
         for j in range(number_of_boxes):
-            x_location = i * (1 + spacing)
-            z_location = j * (1 + spacing)
-            create_box((x_location, 0, z_location), material)
+            color = (random.random(), random.random(), random.random())
+            material = create_material("BoxMaterial", color, alpha)
+            x_pos = i * (1 + spacing) + pos_0[0]
+            y_pos = pos_0[1]
+            z_pos = j * (1 + spacing) + pos_0[2]
+            create_box((x_pos, y_pos, z_pos), material)
 
 
 # Clear existing objects
 bpy.ops.object.select_all(action='SELECT')
 bpy.ops.object.delete()
 
-# Create a row of 5 boxes with a spacing of 0.1 units and 50% opacity
-create_row_of_boxes(5, 0.1, (1, 0, 0), 0.5)  # Red color, 50% opacity
+create_array_of_boxes(5, 0.05)
+create_array_of_boxes(3, 0.05, pos_0=(1,4,1))
+
 
 
